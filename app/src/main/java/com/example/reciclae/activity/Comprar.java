a78befc82ea187dbd102291e1e2b100a05ef8af6
Artur <artur1739@gmail.com>
2020-07-05 03:07:23 -0300

    Buy item and select state spinner implemented.

--- app/src/main/java/com/example/reciclae/activity/Comprar.java
+++ app/src/main/java/com/example/reciclae/activity/Comprar.java
@@ -1,16 +1,21 @@
 package com.example.reciclae.activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.reciclae.R;
+import com.example.reciclae.dao.ClienteDAO;
 import com.example.reciclae.database.AppDatabase;
+import com.example.reciclae.model.Cliente;
 import com.example.reciclae.model.Produto;
 
 import java.util.List;
@@ -19,6 +24,7 @@ public class Comprar extends AppCompatActivity {
 
     private String email;
     private ListView lvComprar;
+    private List MyDataObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +36,28 @@ public class Comprar extends AppCompatActivity {
 
         List<Produto> produtos = AppDatabase.getInstance(getApplicationContext()).produtoDao().getAll();
 
-        ArrayAdapter<Produto> adapter = new ArrayAdapter<Produto>(this, android.R.layout.simple_list_item_1, produtos);
+        final ArrayAdapter<Produto> adapter = new ArrayAdapter<Produto>(this, android.R.layout.simple_list_item_1, produtos);
 
         lvComprar.setAdapter(adapter);
+
+        lvComprar.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                AlertDialog.Builder adb = new AlertDialog.Builder(Comprar.this);
+                adb.setTitle("Comprar");
+                adb.setMessage("Deseja comprar o item " + position);
+                final int positionToRemove = position;
+                adb.setNegativeButton("Cancel", null);
+                adb.setPositiveButton("Comprar", new AlertDialog.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //MyDataObject.remove(positionToRemove);
+                        adapter.remove(adapter.getItem(positionToRemove));
+                        adapter.notifyDataSetChanged();
+                    }});
+                adb.show();
+            }
+        });
     }
+
 
     public void voltar(View view) {
         Intent mainMenu = new Intent(Comprar.this, MainMenu.class);
--- app/src/main/java/com/example/reciclae/activity/SegundoCadastrar.java
+++ app/src/main/java/com/example/reciclae/activity/SegundoCadastrar.java
@@ -1,13 +1,14 @@
 package com.example.reciclae.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
-
 import com.example.reciclae.R;
 import com.example.reciclae.database.AppDatabase;
 import com.example.reciclae.model.Cliente;
@@ -15,8 +16,10 @@ import com.example.reciclae.model.Endereco;
 
 public class SegundoCadastrar extends AppCompatActivity {
 
-    private EditText rua, numero, complemento, bairro, cidade, estado, cep;
+    private EditText rua, numero, complemento, bairro, cidade, cep;
     private String nomeCompleto, usuario, documento, email, senha;
+    private Spinner estadoSpinner;
+    private boolean estadoSelected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +32,39 @@ public class SegundoCadastrar extends AppCompatActivity {
         complemento = findViewById(R.id.complementoCadastrar);
         bairro = findViewById(R.id.bairroCadastrar);
         cidade = findViewById(R.id.cidadeCadastrar);
-        estado = findViewById(R.id.estadoCadastrar);
 
         nomeCompleto = getIntent().getStringExtra("NOME");
         usuario = getIntent().getStringExtra("USER");
         documento = getIntent().getStringExtra("DOC");
         email = getIntent().getStringExtra("EMAIL");
         senha = getIntent().getStringExtra("SENHA");
+
+        estadoSpinner = findViewById(R.id.estadoSpinner);
+        ArrayAdapter<CharSequence> adp = ArrayAdapter.createFromResource(this,
+                R.array.estados, android.R.layout.simple_list_item_1);
+
+        adp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        estadoSpinner.setAdapter(adp);
+        estadoSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+                if(position == 0) {
+                    onNothingSelected(arg0);
+                    estadoSelected = false;
+                } else {
+                    estadoSelected = true;
+                    String ss = estadoSpinner.getSelectedItem().toString();
+                    Toast.makeText(getBaseContext(), ss, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                estadoSelected = false;
+                Toast.makeText(getBaseContext(), "Por favor, selecione um estado!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
@@ -54,13 +83,13 @@ public class SegundoCadastrar extends AppCompatActivity {
         String complementoString = complemento.getText().toString();
         String bairroString = bairro.getText().toString();
         String cidadeString = cidade.getText().toString();
-        String estadoString = estado.getText().toString();
+        String estadoString = estadoSpinner.getSelectedItem().toString();
 
         Cliente cliente = db.clienteDao().findByEmail(email);
         if(cliente != null){
             Toast.makeText(this, "Cliente já cadastrado!", Toast.LENGTH_SHORT).show();
         } else {
-            if(cepString.matches("") || ruaString.matches("") || numeroString.matches("") || bairroString.matches("") || cidadeString.matches("") || estadoString.matches("")){
+            if(cepString.matches("") || ruaString.matches("") || numeroString.matches("") || bairroString.matches("") || cidadeString.matches("") || estadoSelected == false){
                 Toast.makeText(this, "Dados incompletos!", Toast.LENGTH_SHORT).show();
             } else {
                 cliente = new Cliente();
@@ -82,7 +111,7 @@ public class SegundoCadastrar extends AppCompatActivity {
                 endereco.cidade = cidadeString;
                 endereco.estado = estadoString;
                 endereco.fk_idc = idc;
-                //db.clienteDao().insertAllEnderecos(endereco);
+                db.enderecoDao().insertAllEnderecos(endereco);
 
                 Toast.makeText(this, "Cadastro efetuado com sucesso!", Toast.LENGTH_SHORT).show();
                 Intent mainActivity = new Intent(SegundoCadastrar.this, MainActivity.class);
